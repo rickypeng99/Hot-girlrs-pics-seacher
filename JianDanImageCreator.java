b81e3e9b4ec1d8a64c4dcc4db9826982fc0a5f29
Ricky Peng <rickypeng99@gmail.com>
2017-03-04 20:12:31 +0700

    Update JianDanImageCreator.java

--- JianDanImageCreator.java
+++ JianDanImageCreator.java
@@ -10,7 +10,7 @@ public class JianDanImageCreator implements Runnable {
 	private static int count = 0;
 	private String imageUrl;
 	private int page;
-	 //存储路径
+	 //location for saving 
 	private static final String basePath = "E:/jiandan"; 
 	public JianDanImageCreator(String imageUrl,int page) {
 		this.imageUrl = imageUrl;
@@ -21,13 +21,13 @@ public class JianDanImageCreator implements Runnable {
 		File dir = new File(basePath);
 		if(!dir.exists()){
 			dir.mkdirs();
-			System.out.println("图片存放于"+basePath+"目录下");
+			System.out.println("The photo is saved at"+basePath);
 		}
 		String imageName = imageUrl.substring(imageUrl.lastIndexOf("/")+1);
 		try {
 			File file = new File( basePath+"/"+page+"--"+imageName);
 			OutputStream os = new FileOutputStream(file);
-			//创建一个url对象
+			//Creating an url object
 			
 			URL url = new URL("http:"+imageUrl);
 			InputStream is = url.openStream();
@@ -39,10 +39,10 @@ public class JianDanImageCreator implements Runnable {
 				}
 				byte[] temp = new byte[readed];
 				System.arraycopy(buff, 0, temp, 0, readed);
-				//写入文件
+				//inputing the file
 				os.write(temp);
 			}
-			System.out.println("第"+(count++)+"张妹子:"+file.getAbsolutePath());
+			System.out.println("No."+(count++)+" girl:"+file.getAbsolutePath());
 			is.close(); 
             os.close();
 		} catch (Exception e) {
